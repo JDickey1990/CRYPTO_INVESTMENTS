adfe978a7bd7f4db2e2a5780792537d10cb4b3d4
jmd <jordan.michael.dickey@gmail.com>
2020-10-16 15:48:18 -0600

    seeding the database with some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 #create 2 users 
-#User.create(username: "Jack", password: "password")
-#User.create(username: "Jill", password: "password")
+jack = User.create(username: "Jack", password: "password")
+jill = User.create(username: "Jill", password: "password")
 
 #create some investments
-#Coin.create(user_id: "jill.user_id", name: "Bitcoin ", symbol: "btc", quantity: 24)
+
 
 #user AR to pre-associate data
-#jack.investment.create(name: "Bitcoin ", symbol: "btc", quantity: 24)
+jack.coins.create(name: "Bitcoin ", symbol: "btc", quantity: 24)
+jack.coins.create(name: "Ethereum", symbol: "eth", quantity: 100)
+
+jill.coins.create(name: "Decred", symbol: "dcr", quantity: 1000)
+jill.coins.create(name: "Expanse", symbol: "exp", quantity: 5000)

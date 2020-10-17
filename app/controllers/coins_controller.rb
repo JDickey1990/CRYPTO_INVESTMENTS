c74002e69303e774cc17e584af35cbecd1969a5c
jmd <jordan.michael.dickey@gmail.com>
2020-10-17 14:50:57 -0600

    fixed the /coins route to find_by_id and show that users coins

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -2,8 +2,9 @@ class CoiNsController < ApplicationController
 
   # GET: /coins
   get "/coins" do
+  # binding.pry
     if logged_in?
-      @user = User.find_by(session[:user_id])
+      @user = User.find_by_id(session[:user_id])
       @coins = @user.coins.all
       erb :"/coins/index.html"
     else
@@ -19,8 +20,12 @@ class CoiNsController < ApplicationController
   # POST: /coins
   post "/coins" do
     # binding.pry
-    user = User.find_by(session[:user_id])
+    user = User.find_by_id(session[:user_id])
     @coin =user.coins.create(params)
+    e = @coin.amount_invested.to_f
+    f = @coin.quantity
+   @coin.average_coin_price = e/f
+   @coin.save
     redirect "/coins/#{@coin.id}"
   end
 
@@ -38,7 +43,41 @@ class CoiNsController < ApplicationController
 
   # PATCH: /coins/5
   patch "/coins/:id" do
-    redirect "/coins/:id"
+    @coin = Coin.find_by_id(params[:id])
+    @coin.name = params[:name]
+    @coin.symbol = params[:symbol]
+    @coin.quantity = params[:quantity]
+    @coin.amount_invested = params[:amount_invested]
+    @coin.average_coin_price = params[:average_coin_price]
+    @coin.save
+    redirect "/coins/#{@coin.id}"
+  end
+ 
+
+
+    # GET: /coins/:id/purchased
+    get "/coins/:id/purchased" do
+      @coin = Coin.find_by_id(params[:id])
+      erb :"/coins/purchased.html"
+    end
+
+  # PATCH: /coins/5/purchased
+  patch "/coins/:id/purchased" do
+    # binding.pry
+    @coin = Coin.find_by_id(params[:id])
+    a= @coin.quantity 
+    b = params[:quantity].to_d
+    @coin.quantity = a+b
+
+    c= @coin.amount_invested
+    d= params[:cost].to_f
+    @coin.amount_invested = c+d
+  
+    e = @coin.amount_invested
+    f = @coin.quantity
+   @coin.average_coin_price = e/f
+    @coin.save
+    redirect "/coins/#{@coin.id}"
   end
 
   # DELETE: /coins/5/delete

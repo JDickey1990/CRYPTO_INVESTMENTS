746262f648c6ca9889944921e27362ea7ae2a80d
jmd <jordan.michael.dickey@gmail.com>
2020-10-17 14:49:04 -0600

    changed helper methods to be more dynamic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,67 +17,12 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def current_user
-       User.find(session[:user_id])
+       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
     def logged_in?
-        !!session[:user_id]
+        !!current_user
     end
   end  
 
-  get '/signup' do 
-    if logged_in?
-        redirect to '/coins' 
-    else
-        erb :'signup.html'
-    end
-end
-
-post '/signup' do 
-    if params[:username] == "" || params[:password] == ""
-        redirect "/signup"
-    else
-        user =User.new(:username => params[:username], :password => params[:password])	
-        if user.save
-            session[:user_id] = user.id
-            redirect "/coins"
-        else
-            redirect "/signup"
-        end
-    
-    end
-end
-
-  get '/login' do 
-    if logged_in?
-        redirect to '/coins' 
-    else
-        erb :'login.html'
-    end
-  end
-
-  post '/login' do 
-    # binding.pry
-    if params[:username] == "" || params[:password] == "" 
-        redirect "/login"
-    else 
-        user = User.find_by(username: params[:username])
-        if user.save
-            session[:user_id] = user.id
-            redirect "/coins"
-        else
-            redirect "/login"
-        end
-    
-    end
-end
-
-  get '/logout' do 
-    session.clear
-    erb :'logout.html'
-  end
-
-  
- 
-
-end
+ end
